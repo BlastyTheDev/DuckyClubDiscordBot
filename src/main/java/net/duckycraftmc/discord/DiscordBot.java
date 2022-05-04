c7088ed99afddbfb42de1331f56c23ac1524734d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-04 15:32:08 +1000

    Starting Development

--- src/main/java/net/duckycraftmc/discord/DiscordBot.java
+++ src/main/java/net/duckycraftmc/discord/DiscordBot.java
@@ -1,8 +1,14 @@
 package net.duckycraftmc.discord;
 
-public class DiscordBot {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
-    public DiscordBot() {
+import javax.security.auth.login.LoginException;
+
+public final class DiscordBot {
+
+    public DiscordBot() throws LoginException, InterruptedException {
+        JDA jda = JDABuilder.createDefault("OTMyMTMxODc0OTA4ODE1NDEw.YeOhdQ.XKPPj-QElp77BiNXR1_kUacYvDI").build().awaitReady();
     }
 
 }
--- src/main/java/net/duckycraftmc/discord/Main.java
+++ src/main/java/net/duckycraftmc/discord/Main.java
@@ -1,8 +1,10 @@
 package net.duckycraftmc.discord;
 
+import javax.security.auth.login.LoginException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException, InterruptedException {
         new DiscordBot();
     }
 
