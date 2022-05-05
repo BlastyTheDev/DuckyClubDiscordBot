5b81d08492d5316b9aa66c1337c762ce291aa048
BuildTools <unconfigured@null.spigotmc.org>
2022-05-05 11:52:46 +1000

    /announce progress

--- src/main/java/net/duckycraftmc/discord/DiscordBot.java
+++ src/main/java/net/duckycraftmc/discord/DiscordBot.java
@@ -1,9 +1,7 @@
 package net.duckycraftmc.discord;
 
-import net.duckycraftmc.discord.commands.CommandManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
@@ -12,10 +10,7 @@ import javax.security.auth.login.LoginException;
 public final class DiscordBot {
 
     public DiscordBot() throws LoginException, InterruptedException {
-        JDA jda = JDABuilder.createDefault("")
-                .setActivity(Activity.watching("myself become completed"))
-                .addEventListeners(new CommandManager())
-                .build().awaitReady();
+        JDA jda = JDABuilder.createDefault("").build().awaitReady();
         Guild guild = jda.getGuildById("747237441026523147");
         if (guild != null) {
             guild.upsertCommand("serverinfo", "Get the Duckycraft Minecraft server information").queue();
--- src/main/java/net/duckycraftmc/discord/commands/AnnounceCommand.java
+++ src/main/java/net/duckycraftmc/discord/commands/AnnounceCommand.java
@@ -12,8 +12,8 @@ public class AnnounceCommand extends Command {
     @Override
     public void execute(SlashCommandInteractionEvent e) {
         if (e.getInteraction().getMember().getRoles().contains(e.getGuild().getRoleById("747249849535037483"))) {
-
-        }
+            e.reply("you have perms").setEphemeral(true).queue();
+        } else e.reply("Only Owners can use this!").setEphemeral(true).queue();
     }
 
 }
